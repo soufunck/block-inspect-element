c4b8181a5ccea5c9804427bfe2e18a5cae19dbc0
young <62726410+sir-young@users.noreply.github.com>
2020-11-22 10:11:35 -0300

    Bloqueador

---
+++ script.js
@@ -0,0 +1,19 @@
+// Função para bloquear a leitura do menu de contexto
+function offContextMeny() {
+     return false;
+}
+
+document.oncontextmenu = offContextMenu
+
+// Função bloqueando o clique com o botão direito do mouse
+function offRightButton(e) {
+     return false
+}
+
+document.onmousedown = offRightButton
+
+// Bloqueio da combinação "Ctrl + Shift + I" (que abre o Inspencionar Elemento)
+window.onkeydown = function () {
+     const key = event.keyCode || event.charCode || event.which;
+     if (key == 17) { } return false;
+}
